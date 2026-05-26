f818171659061bc1cf9de925bd2bedd27270ce47
miigueprieto <162703099+miigueprieto@users.noreply.github.com>
2026-05-26 22:27:35 +0200

    Update Mediana.py

--- Mediana.py
+++ Mediana.py
@@ -41,12 +41,34 @@ def filtro_mediana(imagen, K=3):
 
 
 
+def calcular_mse(original, procesada):
+    # El Error Cuadrático Medio es la media de las diferencias al cuadrado, cuanto más cerca de 0 mejor
+    return np.mean((original - procesada) ** 2)
+
+def calcular_psnr(original, procesada, max_pixel=1.0):
+    mse = calcular_mse(original, procesada)
+    if mse == 0:
+        return float('inf') # Si no hay error, el PSNR es infinito
+    
+    # Fórmula del PSNR, cuantos menos dB más ruido en la imagen
+    return 10 * np.log10((max_pixel ** 2) / mse)
+
 img_org = io.imread("lena.bmp", as_gray=True)
 
 # Añadimos ruido "Sal y Pimienta" para simular la degradación
 img_ruidosa = util.random_noise(img_org, mode='s&p', amount=0.05)
 
 resultado = filtro_mediana(img_ruidosa, K=3)
+
+mse_ruido = calcular_mse(img_org, img_ruidosa)
+psnr_ruido = calcular_psnr(img_org, img_ruidosa, max_pixel=1.0)
+
+mse_filtro = calcular_mse(img_org, resultado)
+psnr_filtro = calcular_psnr(img_org, resultado, max_pixel=1.0)
+
+print(" FILTRO MEDIANA ")
+print(f"Imagen Ruidosa -> (MSE: {mse_ruido:.4f} | PSNR: {psnr_ruido:.2f} dB)")
+print(f"Imagen Filtrada -> (MSE: {mse_filtro:.4f} | PSNR: {psnr_filtro:.2f} dB)")
 
 # Visualización gráfica
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
@@ -56,14 +78,13 @@ ax1.set_title('1. Imagen Original')
 ax1.axis('off')
 
 ax2.imshow(img_ruidosa, cmap='gray')
-ax2.set_title('2. Degradación: Ruido S&P')
+ax2.set_title('2. Ruido S&P')
 ax2.axis('off')
 
 ax3.imshow(resultado, cmap='gray')
-ax3.set_title('3. Restauración: Filtro Mediana')
+ax3.set_title('3. Restauración Mediana')
 ax3.axis('off')
 
 plt.tight_layout()
 plt.show()
-
  
